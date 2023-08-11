8d03a92dad7aa08ce359971701b715ac4b897ec2
Reed Nelson <reed.nelson.d@gmail.com>
2023-08-11 08:41:16 -0500

    various styling enhancements

--- src/lib/utils/readingTime.ts
+++ src/lib/utils/readingTime.ts
@@ -1,6 +1,6 @@
 // content reading
 const readingTime = (content: string): string => {
-  const WPS = 275 / 60;
+  const WPS = 225 / 60;
 
   let images = 0;
   const regex = /\w/;
@@ -26,14 +26,10 @@ const readingTime = (content: string): string => {
 
   const minutes = Math.ceil(((words - imageAdjust) / WPS + imageSecs) / 60);
 
-  if (minutes < 10) {
-    if (minutes < 2) {
-      return "0" + minutes + ` Min read`;
-    } else {
-      return "0" + minutes + ` Mins read`;
-    }
+  if (minutes < 2) {
+    return minutes + ` minute`;
   } else {
-    return minutes + ` Mins read`;
+    return minutes + ` minutes`;
   }
 };
 
